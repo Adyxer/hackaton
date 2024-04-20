5f98af746b1381540ce6435809ccb4d6dfd93a5d
Adyxer <e.m.zhdanov@yandex.ru>
2024-04-20 21:53:12 +0300

    new

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ class Ui_MainWindow:
 
     def __init__(self, master):
         style = ttk.Style()
-        style.configure('BW.TLabel',background="#A0AECD",foreground='white',relief='')
+        style.configure('BW.TLabel',compound='bottom',background="#A0AECD",foreground='white',relief='ridge')
         self.master = master
         self.master.geometry("859x600")
         self.master.configure(background="#FFFFFF")
@@ -110,14 +110,90 @@ class UI_Abit_Info:
         self.search_2.place(x=482, y=20, width=157, height=25, )
         self.label_2.configure(background="#A0AECD")
         self.search_2.configure(text="Вход")
-        self.search_2.configure(style="B")
+        self.search_2.configure(style="BW.TLabel")
 
         self.search_3 = ttk.Button(self.master)
         self.search_3.place(x=636, y=20, width=156, height=25)
         self.search_3.configure(text="Регистрация")
         self.search_3.configure(style="TButton")
+        self.name_txt=ttk.Label(master)
+        self.name_txt.configure(text='Enter your score on first subject:',background='white')
+        self.name_txt.place(x=20,y=200)
+        self.name=tk.Entry(master)
+        self.name.place(x=280,y=200)
+        self.name_txt = ttk.Label(master)
+        self.name_txt.configure(text='Enter your score on the second subject:', background='white')
+        self.name_txt.place(x=20, y=300)
+        self.name = tk.Entry(master)
+        self.name.place(x=280, y=300)
+        self.name_txt.configure(text='Enter your score on the third subject:', background='white')
+        self.name_txt.place(x=20, y=300)
+        self.name = tk.Entry(master)
+        self.name.place(x=280, y=300)
+        def get_name():
+            return self.name.get()
 
+import json
+def enter_score():
+    score_str = input("Введите баллы за предметы(через запятую):\n")
+    score_input = score_str.strip().split(',')
+    x = False
+    n = 0
+    for j in range(0, len(score_input)):
+        score_input[j] = int(score_input[j])
+    if score_input[0] > 100 or score_input[1] > 100 or score_input[2] > 100:
+            print("Incorrect data! Try again!\n")
+            enter_score()
+    else:
+        return score_input
+file1=open('priem.json','r', encoding='utf-8')
+json_data = file1.read()
+data = json.loads(json_data)
+dest=[]
+p_b=[]
+k_g=[]
+p_k=[]
+price=[]
+min_score=50
+score=[]
+subjects=[]
+konk_b=[]
+konk_k=[]
+print(data)
 
+subjects_input=input("Введите предметы (через запятую):")
+score_input=enter_score()
+
+subjects_input=subjects_input.strip().split(',')
+print(subjects_input)
+print(score_input)
+total_score=0
+for i in score_input:
+    total_score+=int(i)
+for i in data:
+    subjects_temp=[]
+    for j in i:
+        if j=="Направление":
+            dest.append(i[j])
+        if j=="Конкурсн. группа":
+            k_g.append(i[j])
+        if j=="КПЦ_Б":
+            p_b.append(i[j])
+        if j == "КПЦ_К":
+            p_k.append(i[j])
+        if j=="Цена":
+            price.append(i[j])
+        if j in ['Предмет 1','Предмет 2','Предмет 3']:
+            subjects_temp.append(i[j])
+            if j=='Предмет 3':
+                subjects.append(subjects_temp)
+                subjects_temp=[]
+        if j=="Балл":
+            score.append(i[j])
+        if j=="Конкурс_Б":
+            konk_b.append(i[j])
+        if j=="Конкурс_К":
+            konk_k.append(i[j])
 def show_abit_info():
     root = tk.Toplevel()
     ui = UI_Abit_Info(root)
