e0a2a500174e8ff21effacad9262b3012adf7bc6
Adyxer <e.m.zhdanov@yandex.ru>
2024-04-21 09:08:06 +0300

    new

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import tkinter as tk
 from tkinter import ttk
 from tkinter import PhotoImage
+import json
 
 
 class Ui_MainWindow:
 
     def __init__(self, master):
         style = ttk.Style()
-        style.configure('BW.TLabel',compound='bottom',background="#A0AECD",foreground='white',relief='ridge')
+        style.configure('BW.TLabel', compound='bottom', background="#A0AECD", foreground='white', relief='ridge')
         self.master = master
         self.master.geometry("859x600")
         self.master.configure(background="#FFFFFF")
@@ -19,22 +20,22 @@ class Ui_MainWindow:
 
         self.abit = ttk.Button(self.master)
         self.abit.place(x=380, y=90, width=245, height=16)
-        self.abit.configure(text="Абитуриентам", command=lambda: show_abit_info())
+        self.abit.configure(text="Абитуриентам", command=lambda: self.show_abit_info())
         self.abit.configure(style="BW.TLabel")
 
         self.search = ttk.Button(self.master)
         self.search.place(x=680, y=90, width=246, height=30)
-        self.search.configure(text="Поиск по ЕГЭ",command=self.show_student_info)
+        self.search.configure(text="Поиск по ЕГЭ", command=self.show_student_info)
         self.search.configure(style="BW.TLabel")
-        print(self.search.state())
 
         self.label = tk.Label(self.master)
         self.label.place(x=-10, y=-50, width=161, height=181)
         self.img = PhotoImage(file="image\logo.png")
         self.label.configure(image=self.img)
+
         self.stud = ttk.Button(self.master)
         self.stud.place(x=107, y=90, width=246, height=16)
-        self.stud.configure(text="Студентам", command=self.show_student_info())
+        self.stud.configure(text="Студентам", command=self.show_student_info)
         self.stud.configure(style="BW.TLabel")
 
         self.search_2 = ttk.Button(self.master)
@@ -75,6 +76,10 @@ class Ui_MainWindow:
     def show_student_info(self):
         print("Information for Students")
 
+    def show_abit_info(self):
+        self.root = tk.Toplevel(self.master)
+        self.ui = UI_Abit_Info(self.root)
+
 
 class UI_Abit_Info:
     def __init__(self, master):
@@ -88,7 +93,7 @@ class UI_Abit_Info:
 
         self.abit = ttk.Button(master)
         self.abit.place(x=290, y=90, width=245, height=16)
-        self.abit.configure(text="Абитуриентам", command=lambda: show_abit_info())
+        self.abit.configure(text="Абитуриентам", command=lambda: self.show_abit_info())
         self.abit.configure(style="TButton")
 
         self.search = ttk.Button(master)
@@ -116,93 +121,63 @@ class UI_Abit_Info:
         self.search_3.place(x=636, y=20, width=156, height=25)
         self.search_3.configure(text="Регистрация")
         self.search_3.configure(style="TButton")
-        self.name_txt=ttk.Label(master)
-        self.name_txt.configure(text='Enter your score on first subject:',background='white')
-        self.name_txt.place(x=20,y=200)
-        self.name=tk.Entry(master)
-        self.name.place(x=280,y=200)
-        self.name_txt = ttk.Label(master)
-        self.name_txt.configure(text='Enter your score on the second subject:', background='white')
-        self.name_txt.place(x=20, y=300)
-        self.name = tk.Entry(master)
-        self.name.place(x=280, y=300)
-        self.name_txt.configure(text='Enter your score on the third subject:', background='white')
-        self.name_txt.place(x=20, y=300)
-        self.name = tk.Entry(master)
-        self.name.place(x=280, y=300)
-        def get_name():
-            return self.name.get()
 
-import json
-def enter_score():
-    score_str = input("Введите баллы за предметы(через запятую):\n")
-    score_input = score_str.strip().split(',')
-    x = False
-    n = 0
-    for j in range(0, len(score_input)):
-        score_input[j] = int(score_input[j])
-    if score_input[0] > 100 or score_input[1] > 100 or score_input[2] > 100:
+        self.score1_txt = ttk.Label(master)
+        self.score1_txt.configure(text='Enter your score on first subject:', background='white')
+        self.score1_txt.place(x=20, y=200)
+        self.score1 = tk.Entry(master)
+        self.score1.place(x=280, y=200)
+
+        self.score2_txt = ttk.Label(master)
+        self.score2_txt.configure(text='Enter your score on the second subject:', background='white')
+        self.score2_txt.place(x=20, y=250)
+        self.score2 = tk.Entry(master)
+        self.score2.place(x=280, y=250)
+
+        self.score3_txt = ttk.Label(master)
+        self.score3_txt.configure(text='Enter your score on the third subject:', background='white')
+        self.score3_txt.place(x=20, y=300)
+        self.score3 = tk.Entry(master)
+        self.score3.place(x=280, y=300)
+
+        self.calculate_btn = ttk.Button(master, text="Calculate", command=self.calculate_score)
+        self.calculate_btn.place(x=150, y=350)
+
+        self.total_score_label = ttk.Label(master, text="Total Score:")
+        self.total_score_label.place(x=20, y=400)
+
+        self.total_score_value = ttk.Label(master, text="")
+        self.total_score_value.place(x=150, y=400)
+
+    def calculate_score(self):
+        score1 = int(self.score1.get())
+        score2 = int(self.score2.get())
+        score3 = int(self.score3.get())
+
+        if score1 > 100 or score2 > 100 or score3 > 100:
             print("Incorrect data! Try again!\n")
-            enter_score()
-    else:
-        return score_input
-file1=open('priem.json','r', encoding='utf-8')
-json_data = file1.read()
-data = json.loads(json_data)
-dest=[]
-p_b=[]
-k_g=[]
-p_k=[]
-price=[]
-min_score=50
-score=[]
-subjects=[]
-konk_b=[]
-konk_k=[]
-print(data)
-
-subjects_input=input("Введите предметы (через запятую):")
-score_input=enter_score()
-
-subjects_input=subjects_input.strip().split(',')
-print(subjects_input)
-print(score_input)
-total_score=0
-for i in score_input:
-    total_score+=int(i)
-for i in data:
-    subjects_temp=[]
-    for j in i:
-        if j=="Направление":
-            dest.append(i[j])
-        if j=="Конкурсн. группа":
-            k_g.append(i[j])
-        if j=="КПЦ_Б":
-            p_b.append(i[j])
-        if j == "КПЦ_К":
-            p_k.append(i[j])
-        if j=="Цена":
-            price.append(i[j])
-        if j in ['Предмет 1','Предмет 2','Предмет 3']:
-            subjects_temp.append(i[j])
-            if j=='Предмет 3':
-                subjects.append(subjects_temp)
-                subjects_temp=[]
-        if j=="Балл":
-            score.append(i[j])
-        if j=="Конкурс_Б":
-            konk_b.append(i[j])
-        if j=="Конкурс_К":
-            konk_k.append(i[j])
-def show_abit_info():
-    root = tk.Toplevel()
-    ui = UI_Abit_Info(root)
-    root.mainloop()
+        else:
+            total_score = score1 + score2 + score3
+            self.total_score_value.configure(text=str(total_score))
 
 
 def main():
     root = tk.Tk()
     ui = Ui_MainWindow(root)
+    file1 = open('priem.json', 'r', encoding='utf-8')
+    json_data = file1.read()
+    data = json.loads(json_data)
+    dest = []
+    p_b = []
+    k_g = []
+    p_k = []
+    price = []
+    min_score = 50
+    score = []
+    subjects = []
+    konk_b = []
+    konk_k = []
+    print(data)
     root.mainloop()
 
 
